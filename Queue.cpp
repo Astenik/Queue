45ea5d96e3996c61932717a80dbd077d6aec8e9e
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-04 18:05:45 +0400

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -2,19 +2,18 @@
 #include "ListNode.h"
 
 template <typename T>
-
 class Queue 
 {
  public:
     virtual  ~Queue();
     virtual void enqueue(const T&) = 0;
     virtual void dequeue() = 0;
-    virtual bool is_empty()const = 0;
+    virtual bool is_empty() const = 0;
     virtual void clear() = 0;
+    virtual T front() const = 0;
 }; 
 
 template <typename T> 
-
 Queue<T>::~Queue()
 {
     std::cout << "Queue destructor" << std::endl;
