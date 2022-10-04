82520271e7026de63dab3734fd9ec926a1606e30
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-04 18:09:31 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Lqueue.h"
+
+int main()
+{
+    Lqueue<int> obj;
+    std::cout << obj.is_empty() << std::endl;
+    
+    obj.enqueue(78);
+    obj.enqueue(23);
+    obj.enqueue(89);
+    obj.dequeue();
+    std::cout << obj.front() << std::endl;
+    
+    Lqueue<int> k;
+    k = obj;
+    std::cout << k.is_empty() << std::endl;
+    std::cout << k.front() << std::endl;
+    obj.clear();
+    return 0;
+}
